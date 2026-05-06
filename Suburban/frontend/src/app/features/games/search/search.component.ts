602f41578b1411a937269f92665fefb7e5f99585
Abdulrhman Bahaa <abdulrhman.bahaa85@gmail.com>
2026-05-06 22:22:10 +0300

    chore: removing commented code

--- Suburban/frontend/src/app/features/games/search/search.component.ts
+++ Suburban/frontend/src/app/features/games/search/search.component.ts
@@ -24,11 +24,6 @@ export class SearchComponent {
     constructor(private router: Router, private gamesService: GamesService, private cdRef: ChangeDetectorRef) { }
 
     onSearch(query: string): void {
-        // Implement search logic here, for example:
-        // this.games$ = this.gamesService.getGames({ page: 1, pageSize: 1, query: query });
-        // console.log(`Searching for games with query: ${query}`);
-        // this.cdRef.detectChanges();
-
         this.gamesService.getGames({ page: 1, pageSize: 20, query: query }).subscribe(games => {
             this.games$ = of(games);
             console.log(games, query);
